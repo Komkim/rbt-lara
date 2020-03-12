970bc6f1e38c46d5dccc9b065a46e736305f0e39
komkim <komkim@mail.ru>
2020-03-12 03:32:39 +0300

    night commit

--- database/factories/NewsFactory.php
+++ database/factories/NewsFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(News::class, function (Faker $faker) {
 
     $title = $faker->text(rand(5,15));
-    $text = $faker->realText(rand(10,500));
+    $text = $faker->realText(rand(1000,5000));
     $description = mb_strlen($text)>30 ? mb_substr($text, 0, 30) . '...' : $text;
     $created = $faker->dateTimeBetween('-30 days', '-1 days');
 
